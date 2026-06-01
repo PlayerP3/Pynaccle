6998b6430923d715d761bf6b15f0b90d698febba
PlayerP3 <140167984+PlayerP3@users.noreply.github.com>
2026-06-02 01:56:20 +0200

    engine only rework

---
+++ pynaccle/engine.py
@@ -0,0 +1,96 @@
+import pygame,random,os,string,numpy,math,json,copy,ast,sys
+from pygame.math import Vector2
+from .statemachine import StateMachine
+from .screen import gameScreen
+from .objectsystem import objectManager
+from .eventsystem import eventprocessor
+from .pens import penHolder
+from .tilemap import tilemapProcessor
+from .hud import overlay
+
+pygame.font.init()
+
+class GameStateMachine(StateMachine):
+
+    def __init__(self):
+
+        StateMachine.__init__(self)
+
+    def update(self):
+
+        # get all events
+        eventprocessor.events = pygame.event.get()
+
+        # handle events
+        eventprocessor.process_base_events()
+
+
+        self.state.update()
+
+        if self.state.done:
+            self.transition_to_next_state()
+
+
+class Engine(GameStateMachine):
+
+    def __init__(self):
+
+        # vars for running the game
+        self.playing = True
+
+        self.clock = pygame.time.Clock()
+
+        # get managers
+        self.eventprocessor = eventprocessor
+        self.objectManager = objectManager
+        self.screenManager = gameScreen
+        self.penHolder = penHolder
+        self.tilemapProcessor = tilemapProcessor
+        self.overlay = overlay
+
+        # create vars
+        self.tileSize = None
+
+        self.gamePath = None
+
+    # def init(self,states:dict={},beginningState:str='START',tilemapPath:str='',classMappings:dict={},player:object=None,tileSize:int=32):
+    def init(self,states:dict={},beginningState:str='SPLASH',tilemapJSON:str='',classMappings:dict={},
+             windows:str='configs/config_window.json'):
+
+        # set path to game
+        # self.gamePath = gamePath
+
+        # set states
+        self.states = states
+
+        # set parent node for player states
+        for x in self.states:
+            self.states[x].parent_node = self
+    
+        # set first state and enter
+        self.state = self.states[beginningState]
+
+        # add windows to screen
+        with open(windows,'r') as winConfig:
+
+            winParams = json.load(winConfig)
+
+            for winName in winParams:
+                self.screenManager.add_window(winName=winName,width=winParams[winName]["width"],height=winParams[winName]["height"],
+                                              zoom=winParams[winName]["zoom"],pos=(self.screenManager.fullscreen_width//2,self.screenManager.fullscreen_height//2))
+
+
+        # process tilemap
+        self.tilemapProcessor.load_tilemap(tileampJSON=tilemapJSON,classMappings=classMappings)
+
+    def run(self):
+
+        if self.playing:
+            self.update()
+        
+        
+
+
+        
+            
+
---
+++ pynaccle/tilemap.py
@@ -0,0 +1,115 @@
+import pygame,os,json,ast,sys
+from .screen import gameScreen
+from .objectsystem import objectManager
+
+class Tilemap():
+    def __init__(self):
+
+
+        self.accessible_tiles = []
+        self.inacessible_tiles = []
+        self.tilemap = {}
+        self.astar_graph = {}
+
+        pass
+
+
+    def load_tilemap(self,tileampJSON:str,classMappings:dict):
+
+        with open(tileampJSON, 'r') as f:
+            
+            params = json.load(f)
+
+        # add window for chunk
+        # set chunk in bg surf
+        gameScreen.add_window('Chunk1',width=3200,height=3200,pos=(gameScreen.fullscreen_width//2,gameScreen.fullscreen_height//2),zoom=gameScreen.windows['win'].zoom)
+        gameScreen.windows['Chunk1'].bg_offset_x = gameScreen.windows['Chunk1'].win_width//2 
+        gameScreen.windows['Chunk1'].bg_offset_y = gameScreen.windows['Chunk1'].win_height//2 
+
+
+        # store all pos and layers
+        layerPos = []
+
+        # store layer and pos as kv pair
+        for layer,layerData in params.items():
+
+            for pos,metadata in layerData.items():
+
+                layerPos.append((layer,pos))
+
+        # go through kv pair and remove animated sprite class and ad vars you want
+        for lp in layerPos:
+
+            buildJSON = {}
+
+            layer = lp[0]
+            pos = lp[1]
+
+            # get walls
+            if params[layer][pos]['AnimatedSprite']['img_path'].split('/')[-1] == 'Wall.png':
+                params[layer][pos]['class'] = 'Wall'
+
+
+            # get class to convert to
+            className = params[layer][pos]['class']
+            classConversion = params[layer][pos]['class']
+            del params[layer][pos]['class']
+
+            # get sprite obj
+            objinit = params[layer][pos]
+
+            buildJSON.update(objinit['AnimatedSprite'])
+            del objinit['AnimatedSprite']
+            buildJSON.update(objinit)
+
+            # add variables of interest from the animated sprite class, can actuall use getattr to be more efficient and have a list of vars you want
+            newObj = classMappings[classConversion]()
+            for att,val in buildJSON.items():
+                setattr(newObj,att,val)
+
+            # sprite.surface_to_draw_on = 'tilemap'
+            # sprite.vertice = 'topleft'
+            newObj.hurtbox.topleft= ast.literal_eval(pos)
+            newObj.zlayer_drawing = int(layer)
+            newObj.spawnLocation = ast.literal_eval(pos)
+
+            # start building copy
+            if layer not in self.tilemap:
+                self.tilemap[layer] = {} 
+            
+            if pos not in self.tilemap[layer]:
+                self.tilemap[layer][pos] = {}
+
+            # add animated sprite info to myCopy
+            self.tilemap[layer][pos] = newObj
+
+            
+            # determine what happens to different objs
+            if className == 'BgTile':
+
+                newObj.surface_to_draw_on = 'Chunk1'
+                newObj.init_sprite()
+
+                posss = ast.literal_eval(pos)
+                newObj.draw_surface(position=ast.literal_eval(pos),schedule_deletion=False)
+
+                # objectManager.active_pool.append(newObj)
+                # gameScreen.bgSurface['Chunk1'].blit(newObj.sprite,pos)
+            
+
+            else:
+
+                # if className in ['Door','Wallbuy']:
+                #     print(pos)
+                #     sys.exit()
+                newObj.is_active = True
+                objectManager.active_pool.append(newObj)
+        # print(self.tilemap)
+
+
+
+        gameScreen.windows['Chunk1'].render_objects()
+
+
+
+tilemapProcessor = Tilemap()
